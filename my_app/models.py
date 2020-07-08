debd2d5fad9e2017db6bcf8490a4c50eb610f60b
EricksonLopes <ofc.erickson@gmail.com>
2020-07-08 14:59:46 -0300

    renaming post names to administrative area

--- my_app/models.py
+++ my_app/models.py
@@ -7,15 +7,24 @@ from django.db import models
 class Post(models.Model):
     # campos do bando de dados
     # e colocando o tipo do registro através do model.
-    title = models.CharField(max_length=255)  # max_length=255->maximo de caracteres
-    content = models.TextField()  # TextField -> não tem limite de caractere
+    title = models.CharField(max_length=255, verbose_name='Título')  # max_length=255->maximo de caracteres
+    content = models.TextField(verbose_name='Corpo do Artigo')  # TextField -> não tem limite de caractere
     # null para DB
     # blank para formulário
-    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    subtitle = models.CharField(max_length=255, blank=True, null=True, verbose_name='Subtitulo')
 
+    class Meta:
+        # mudar "post" para artigos
+        verbose_name = 'Artigo'
+        # caso o queira mudar o nome quando for plural
+        # verbose_name_plural = 'Artigos'
+
+    def __str__(self):
+        return self.title
 # após fazer alterações ou adicões no banco(
 # python manage.py makemigrations my_app
 # e
 # python manege.py migrate my_app)
 
 # Para saber o sql ( python manage.py sqlmigrate my_app 0001 )
+# para ver as migrações e qual esta ativada (python manage.py showmigrations)
