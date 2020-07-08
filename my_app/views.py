5632992b87225f1aef3f27192fbae7849102f13f
EricksonLopes <ofc.erickson@gmail.com>
2020-07-08 20:32:24 -0300

    filters with get parameters, end of course

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, Category
 
 # MVC - Model View Controller
 # M - models.py
@@ -19,14 +19,35 @@ def home(request):
 
 
 def post(request, post_id):
+    # procura o id dentro do DB
+
     posts = Post.objects.get(id=post_id)
-    return render(request, 'post.html', {'post':posts})
+    categories = Category.objects.all()
+    return render(request, 'post.html',
+                  {
+                      'post': posts,
+                      'categories':categories
+                  })
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    # verifica se a requisição vem com 'category_id'
+    if 'category_id' in request.GET:
+        # busca os post que tenha o id da categoria
+        category = Category.objects.get(id=request.GET['category_id'])
+        # filtra todos os post que tenha essa categoria
+        posts = Post.objects.filter(categories=category)
+    else:
+        # caso nao venha retorna todos os posts
+        posts = Post.objects.all()
+    # procura todas as categorias
+    categories = Category.objects.all()
 
-    return render(request, 'post_list.html', {'posts':posts})
+    return render(request, 'post_list.html',
+                  {
+                      'posts': posts,
+                      'categories': categories
+                  })
 
     # name = 'luiz Carlos'
     # return render(request, 'post_list.html', {'name': name})
